847715ee4c929d014cae91f20aa4abb242e4acb6
subhadip <matainja0131@gmail.com>
2026-07-15 19:07:01 +0530

    Update page.js

--- app/questions/page.js
+++ app/questions/page.js
@@ -3,7 +3,7 @@ import { getAllQuestions } from "../services/questions";
 
 export default async function QuestionsPage() {
     const questions = await getAllQuestions();
-
+ 
     return (
         <main className="p-6">
             <h1 className="mb-6 text-2xl font-bold">All Questions</h1>
